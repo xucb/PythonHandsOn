85302299daff2e49476d4a8522d0d1e59ef747f3
xucb <bill_xu@139.com>
2023-06-12 22:22:10 +0800

    commit

---
+++ PythonHandson/7/7b.py
@@ -0,0 +1,22 @@
+
+
+#7-5
+prompt="Please let me know your age:\n"
+return_message = "please pay "
+your_age =''
+while  your_age!='quit':
+    your_age = input(prompt)
+    if your_age == 'quit':
+        break
+    else:
+        your_age_number = int(your_age)
+        if your_age_number < 3:
+            fee = 0
+        elif your_age_number >12:
+            fee = 15
+        else:
+            fee = 10
+    print(f"{return_message} {fee} dollar.")
+
+#7-6
+
