7b177191ba791d21b91cc7e74a02b5bb5be2fb13
xucb <bill_xu@139.com>
2023-06-25 09:45:41 +0800

    commit

--- PythonHandson/9/9d.py
+++ PythonHandson/9/9d.py
@@ -46,6 +46,8 @@ class Lottery:
 my_lottery = Lottery()
 my_lottery.lucky_contents()
 
+ 
+
 
 #9-15
 from random import choice
@@ -60,7 +62,7 @@ class Lottery:
         """中奖的彩票号码"""
         time = 1
         lucky_contents=[]
-        while time<=4:
+        while time<=5:
             lucky_content= choice(self.contents)
             lucky_contents.append(lucky_content)
             #print(f"第{time}位中奖号码是{lucky_content}.")
@@ -71,31 +73,24 @@ class Lottery:
         #print(f"中奖号码是{lucky_contents}")
         return lucky_contents
 
-def my_ticket():
-    """每次购买彩票的号码"""    
-    time = 1
-    my_tickets=[]
-    my_ticket =''
-    while time<=4:
-        my_ticket = choice(my_lottery.contents)
-        my_tickets.append(my_ticket)
-#        print(f"你购买的彩票第{time}位号码是{my_ticket}.")
-        time +=1
-    return my_tickets
-
 
 my_lottery = Lottery()
 print("中奖号码是：")
-print(my_lottery.lucky_contents())
-print(my_ticket())
+lucky_number = my_lottery.lucky_contents()[:]
+print(lucky_number)
+#print(my_ticket())
 
 
-
-
-
+my_last_ticket=[] 
 cycle_time = 0
-while my_lottery.lucky_contents() == my_ticket():
-    print(f"你中奖了！！！，这是第{cycle_time}次循环。")
-    my_ticket()
-    cycle_time +=1
 
+while True:
+    my_last_ticket=my_lottery.lucky_contents()[:]
+    cycle_time +=1
+    if lucky_number==my_last_ticket:
+        break
+   
+    
+print(f"lucky number is {lucky_number}, your ticket number is {my_last_ticket}")
+
+print(f"你中奖了！！！，这是第{cycle_time}次循环。")
