4595298d88343e41907adf303e220af950d713ce
xucb <bill_xu@139.com>
2023-06-27 21:13:03 +0800

    committed

--- PythonHandson/10/10c.py
+++ PythonHandson/10/10c.py
@@ -3,18 +3,27 @@
 def input_number():
     """用户输入数字"""
     while True:
-        usernumber = input('Please enter a number, input "q" if you want to quit: ')
-        if usernumber == 'q':
-            break
-        else:
-            try:
-                usernumber = int(usernumber)
+        usernumber = input('Please enter a number: ')
+        try:
+            usernumber = int(usernumber)
             break
         except ValueError:
-        print(f"This is not a valid number, please input a valid number.")
+            print(f"This is not a valid number, please input a valid number.")
         continue
     return usernumber
 
+while True:
+    number1 = input_number()
+    number2 = input_number()
+    print(f"{number1} + {number2} = {number1 + number2} \n") 
+    print("Do you want to continue? Enter 'q' to quit.")
+    again = input("")
+    if again == 'q':
+        break
+
+
+"""
+# 另外一种方式,复杂.
 times = 0
 numbers =[]
 while True:
@@ -28,7 +37,8 @@ while True:
 sum = 0
 for number in numbers:
     sum += int(number)
-print(f"the sum of your inputer is {sum}.")
+print(f"the sum of your input is {sum}.")
+"""
 
 
 
