54ce4248bea51e06f22196141bec5d3fb55de6e7
xucb <bill_xu@139.com>
2023-06-30 16:34:52 +0800

    commit

--- PythonHandson/10/a.py
+++ PythonHandson/10/a.py
@@ -5,7 +5,13 @@ import random
 
 def genCode(length):
 	s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-	return "".join(random.sample(s,length))
+	a=random.sample(s,length)
+	print(a)
+	return "".join(a)
    
 l=input("Enter the length of the random code: ")
 print(genCode(int(l)))
+
+s=[[i for i in range(3)] for j in range(4)]
+print(s)
+
